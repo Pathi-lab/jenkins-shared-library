8e1c3ba7f965344b3b67184c1509752e03d411e4
Sunny_ANR <77866066+Pathi-lab@users.noreply.github.com>
2021-10-27 17:24:22 +0530

    Update Zot.groovy

--- src/org/foo/Zot.groovy
+++ src/org/foo/Zot.groovy
@@ -2,7 +2,7 @@ package org.foo
 
 def checkOutFrom(repo) {
   //git url: "https://github.com/Pathi-lab/${repo}.git"
-  sh 'git clone https://github.com/Pathi-lab/${repo}.git'
+  sh 'git clone https://Pathi-lab:apparatus%40%A@github.com/Pathi-lab/${repo}.git'
 }
 
 return this
